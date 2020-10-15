8d0f7b514f9f40daa9885fc6890649b57a3b8811
altruant <noellehamberis@gmail.com>
2020-10-15 16:05:54 -0400

    created api views for model

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Article
+
+from .serializers import ArticleSerializer
+
+
+class ArticleListView(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView)
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
