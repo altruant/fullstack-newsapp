d64f6cfaa1dde41b296772c89c115084116a411c
altruant <noellehamberis@gmail.com>
2020-10-19 11:42:02 -0400

    refactoring

--- frontend/homepage/src/components/ArticleDetail.js
+++ frontend/homepage/src/components/ArticleDetail.js
@@ -1,15 +1,42 @@
 import React from 'react';
-
+import e2020 from '../assets/e2020.png'
+import entertainment from '../assets/entertainment.png'
+import arrowDown from '../assets/arrow-down.svg'
 class ArticleDetail extends React.Component {
+
+
   render() {
+    let icon = ''
+    switch (this.props.article.categories) {
+      case 'Election 2020':
+        icon = e2020;
+        break;
+      case 'Entertainment':
+        icon = entertainment
+        break;
+      default:
+        icon = ''
+    }
     return(
-      <div className="card-body article-detial">
-        <img src={this.props.article.image_url} alt="#"/>
-        <h3>{this.props.article.title}</h3>
-        <span>{this.props.article.categories}</span>
-        <p>{this.props.article.body}</p>
-        <span>Posted on {this.props.article.created}</span>
-        <span>Modified at {this.props.article.modified}</span>
+      <div className="article-detail">
+        <div className="article-header">
+          <img className='detail-img' src={this.props.article.image_url} style={{maxWidth: '100%'}} alt="#"/>
+          <h3>{this.props.article.title}</h3>
+          <div className="article-header-bottom">
+            <div className="icon-categories">
+              <img src={icon} alt="#" className="icon"/>
+              <h3>{this.props.article.categories}</h3>
+            </div>
+            <div className="arrow">
+              <img src={arrowDown} alt='#' className='arrow-down'/>
+            </div>
+          </div>
+          <span>Posted on {this.props.article.created}</span>
+          <span>Modified at {this.props.article.modified}</span>
+        </div>
+        <div className='article-body'>
+          <p>{this.props.article.body}</p>
+        </div>
       </div>
     )
   }
